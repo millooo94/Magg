f6e7c0f2940716fdb12e74cde6694289b5782abf
gmannella <gmannella@ads.it>
2024-06-24 17:06:03 +0200

    Sistemata Index inziale

--- src/main/viewmodels/com/powerservice/managermag/IndexViewModel.java
+++ src/main/viewmodels/com/powerservice/managermag/IndexViewModel.java
@@ -3,12 +3,12 @@ package com.powerservice.managermag;
 import org.zkoss.bind.annotation.*;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zkplus.spring.DelegatingVariableResolver;
 import org.zkoss.zul.Window;
 
 @VariableResolver(DelegatingVariableResolver.class)
 public class IndexViewModel {
-
 
 
 
--- src/main/viewmodels/com/powerservice/managermag/SettingsModalViewModel.java
+++ src/main/viewmodels/com/powerservice/managermag/SettingsModalViewModel.java
@@ -1,8 +1,26 @@
 package com.powerservice.managermag;
 
+import it.powerservice.managermag.ImpostazioniGridService;
+import it.powerservice.managermag.customClass.CustomImpostazioniRow;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.Init;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zkplus.spring.DelegatingVariableResolver;
+import org.zkoss.zul.Window;
+
+import java.util.List;
 
 @VariableResolver(DelegatingVariableResolver.class)
 public class SettingsModalViewModel {
+    @WireVariable
+    ImpostazioniGridService impostazioniGridService;
+
+    List<CustomImpostazioniRow> impostazioniRows;
+
+    @Init
+    public void init(@ContextParam(ContextType.COMPONENT) Window w) throws Exception {
+        impostazioniRows = impostazioniGridService.getImpostazioniRows();
+    }
 }
